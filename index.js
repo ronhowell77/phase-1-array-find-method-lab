a624cccd1c961abd880e3c76a350c26759861488
Ronnie <ron.howell0326@gmail.com>
2022-10-08 14:33:00 -0400

    commit tree 062ef5fa68d97245b102bd77bea5c0047caf20d7

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// code your solution here
+
+function superbowlWin(records){
+    // for(const record of records){
+    // console.log('record', record)
+    //     if(record.result === "W"){
+    //     return record.year   
+    // }
+    // }
+    const found = records.find(function(record){
+        console.log('record', record)
+        return record.result === "W"
+    });
+    if (found){
+        return found.year
+    }
+    console.log('found', found)
+    //[record].find(superbowlWin).year;
+}
